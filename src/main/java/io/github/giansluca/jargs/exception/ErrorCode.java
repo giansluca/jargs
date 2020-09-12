f8d1b83c2e1ad53d3d0ab98bdef553447922150c
gians <giansluca@gmail.com>
2020-09-12 13:18:29 +0200

    Refactoring ErrorCode Enum

--- src/main/java/io/github/giansluca/jargs/exception/ErrorCode.java
+++ src/main/java/io/github/giansluca/jargs/exception/ErrorCode.java
@@ -1,25 +1,14 @@
 package io.github.giansluca.jargs.exception;
 
 public enum ErrorCode {
-    OK("ok"),
-    INVALID_SCHEMA_ELEMENT_TYPE("Invalid schema element type"),
-    EMPTY_SCHEMA_ELEMENT_NAME("Empty schema element name"),
-    INVALID_SCHEMA_ELEMENT_NAME("Invalid schema name"),
-    UNEXPECTED_ARGUMENT("Unexpected argument"),
-    INVALID_ARGUMENT_NAME("Invalid argument name"),
-    MISSING_STRING("Missing string"),
-    MISSING_INTEGER("Missing integer"),
-    INVALID_INTEGER("Invalid integer"),
-    MISSING_DOUBLE("Missing double"),
-    INVALID_DOUBLE("Invalid double");
-
-    private final String description;
-
-    ErrorCode(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+    INVALID_SCHEMA_ELEMENT_TYPE,
+    EMPTY_SCHEMA_ELEMENT_NAME,
+    INVALID_SCHEMA_ELEMENT_NAME,
+    UNEXPECTED_ARGUMENT,
+    INVALID_ARGUMENT_NAME,
+    MISSING_STRING,
+    MISSING_INTEGER,
+    INVALID_INTEGER,
+    MISSING_DOUBLE,
+    INVALID_DOUBLE
 }
