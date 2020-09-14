0de9cb95467c3845feed8c42035b060dbd3e9cee
gians <giansluca@gmail.com>
2020-09-14 10:42:25 +0200

    Added Main test class and updated Readme file

---
+++ src/test/java/io/github/giansluca/jargs/Main.java
@@ -0,0 +1,22 @@
+package io.github.giansluca.jargs;
+
+import io.github.giansluca.jargs.exception.JargsException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String schema = "first, second#, third%, forth@";
+        Jargs arguments = null;
+
+        try {
+            arguments = new Jargs(schema, args);
+        } catch (JargsException e) {
+            e.printStackTrace();
+        }
+
+        String stringValue = arguments.getString("first");
+        int intValue = arguments.getInt("second");
+        boolean boolValue = arguments.getBoolean("third");
+        double doubleValue = arguments.getDouble("forth");
+    }
+}
