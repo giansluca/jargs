66237b10ffe6da65a27934ea927d554b0b4899d4
gians <giansluca@gmail.com>
2026-03-02 10:11:37 +0100

    chore: update libs

--- src/main/java/io/github/giansluca/jargs/exception/JargsArgumentException.java
+++ src/main/java/io/github/giansluca/jargs/exception/JargsArgumentException.java
@@ -40,26 +40,19 @@ public class JargsArgumentException extends JargsException {
 
     @Override
     public String getMessage() {
-        switch (errorCode) {
-            case UNEXPECTED_ARGUMENT:
-                return String.format("Argument name -%s unexpected.", errorArgumentName);
-            case INVALID_ARGUMENT_NAME:
-                return String.format("'%s' is not a valid argument name.", errorArgumentName);
-            case MISSING_STRING:
-                return String.format("Could not find string argument value for '-%s' .", errorArgumentName);
-            case MISSING_INTEGER:
-                return String.format("Could not find integer argument value for '-%s' .", errorArgumentName);
-            case INVALID_INTEGER:
-                return String.format(
-                        "Argument name '-%s' expects an integer but was '%s' .", errorArgumentName, errorParameter);
-            case MISSING_DOUBLE:
-                return String.format("Could not find double argument value for '-%s' .", errorArgumentName);
-            case INVALID_DOUBLE:
-                return String.format(
-                        "Argument name '-%s' expects a double but was '%s' .", errorArgumentName, errorParameter);
-            default:
-                return "";
-        }
+        return switch (errorCode) {
+            case UNEXPECTED_ARGUMENT -> String.format("Argument name -%s unexpected.", errorArgumentName);
+            case INVALID_ARGUMENT_NAME -> String.format("'%s' is not a valid argument name.", errorArgumentName);
+            case MISSING_STRING -> String.format("Could not find string argument value for '-%s' .", errorArgumentName);
+            case MISSING_INTEGER ->
+                    String.format("Could not find integer argument value for '-%s' .", errorArgumentName);
+            case INVALID_INTEGER -> String.format(
+                    "Argument name '-%s' expects an integer but was '%s' .", errorArgumentName, errorParameter);
+            case MISSING_DOUBLE -> String.format("Could not find double argument value for '-%s' .", errorArgumentName);
+            case INVALID_DOUBLE -> String.format(
+                    "Argument name '-%s' expects a double but was '%s' .", errorArgumentName, errorParameter);
+            default -> "";
+        };
     }
 
 }
